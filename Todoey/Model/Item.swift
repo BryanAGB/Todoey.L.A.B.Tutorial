5519053b09f12c2a1315cc548a759aca72d3323c
BryanAGB <bryan@bryanmansell.co.uk>
2017-12-31 11:10:40 +0000

    convert MVC model

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Bryan Mansell on 31/12/2017.
+//  Copyright © 2017 Bryan Mansell. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
